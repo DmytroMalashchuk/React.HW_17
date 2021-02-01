864ec1c191e60313b48ecb3b24a7196061671491
Дмитрий Малащук <dmytro.malashchuk@nure.ua>
2021-02-01 18:50:55 +0200

    commit 222

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Timer from './components/Timer.js';
 function App() {
   return (
     <div >
-      <Timer key={0} time={10} step={100} autostart={false} />
-      <Timer key={1} time={20} step={1000} autostart={true} />
+      <Timer id={1} key={0} time={10} step={100} autostart={false} />
+      <Timer id={2} key={1} time={20} step={1000} autostart={true} />
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './timer.css'
 
-const Timer = ({time, step, autostart}) => {
+const Timer = ({id, time, step, autostart}) => {
     const [counter, setCounter] = useState(time)
     const [start, setStart] = useState(autostart)
     const interval = step / 1000;
@@ -9,13 +9,15 @@ const Timer = ({time, step, autostart}) => {
         const timer = setTimeout(() =>{
             if(counter > 0 && start === true){
                 setCounter(counter - interval)
+            console.log('Залишилось часу на ' + id + '-му таймері: '  + counter.toFixed(2))
         }}, step)
         return timer;
     });
     return (<div className="display">
             <button  onClick = {() => setStart(true)}>Старт</button> 
             <button onClick = {() => setStart(false)}>Пауза</button>
-            <div className="time"> {Number.isInteger(counter)?counter:counter.toFixed(1)} </div>
+            <div className="time"> {(Number.isInteger(counter)?counter:counter.toFixed(1)) > 0?
+            (Number.isInteger(counter)?counter:counter.toFixed(1)):0} </div>
     </div>
     )
 }
